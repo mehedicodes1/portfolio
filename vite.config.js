c1c24a0add16b9b4363ee8a72f8df4304ca501da
Mehedi Hasan <mehedi-dev@outlook.com>
2025-02-06 04:33:13 +0600

    Update1

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
-  base: '/Portfolio/', // This must match your GitHub repo name
+  plugins: [react(), tailwindcss()],
+  base: '/portfolio-website/', // This must match your GitHub repo name
   build: {
     outDir: 'dist',
   },
-  
 })
