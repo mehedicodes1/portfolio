b2af7cb687f77b540dbb8822da5cbafb09e339f0
Mehedi Hasan <mehedi-dev@outlook.com>
2025-02-06 14:20:47 +0600

    Update repository name and configurations for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/portfolio-website/', // This must match your GitHub repo name
+  base: '/portfolio/', // This must match your new GitHub repo name
   build: {
     outDir: 'dist',
   },
